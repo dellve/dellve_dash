46ffb2634644e7aee51c797479f16844df157be0
Abigail Johnson <abigailjohnson@utexas.edu>
2017-03-22 14:03:10 -0400

    removed portal home whitelist

--- app.py
+++ app.py
@@ -51,36 +51,29 @@ def apply_template(template_path, args):
 # Verify that user provided server configuration has
 # a proper dellve installation and dependecies
 def valid_config_params(params):
-    #return True if netdata_enabled(params) and dellve_enabled(params) else False
-    # TODO: remove below and uncomment above once method properly implemented/tested
-    return True if (params[SERVER_TAG] == WHITE_LIST[0][SERVER_TAG] and params[NETDATA_TAG]
-        == WHITE_LIST[0][NETDATA_TAG]) else False
+    return True if dellve_enabled(params) and netdata_enabled(params) else False
 
 # Helper Function to determine whether a server has
 # a proper netdata installation and netdata plugin
 def netdata_enabled(params):
     url = 'http://' + str(params[SERVER_TAG]) + ':' + str(params[NETDATA_TAG]) + NETDATA_SUFFIX
+    print(url)
     return valid_api_endpoint(url)
 
 # Helper Function to determine whether a server has
 # a proper netdata
 def dellve_enabled(params):
+    return True # TODO: uncomment once API deployed
     url = 'http://' + str(params[SERVER_TAG]) + ':' + str(params[DELLVE_TAG]) + DVE_BENCH_LIST
     return valid_api_endpoint(url)
 
 # Helper function to test for existence of HTTP endpoint
-# TODO: implement/test
 def valid_api_endpoint(url):
-    #return True
-    #r = requests.get(url)
-    #return True if requests.get(url).status_code == 200 else False
     try:
         r = requests.get(url)
-        return True if r.status_code == 200 else False
-    #    r.raise_for_status()
-    #    return True
-    # Destination not founds
-    except: # requests.exceptions.HTTPError as err:
+        return True
+    # Destination not found
+    except:
         return False
 
 
--- conf.py
+++ conf.py
@@ -10,7 +10,7 @@ DELLVE_TAG = 'dellve_port'
 NETDATA_TAG = 'netdata_port'
 
 # External API Endpoints used for dependency verification and form submission
-NETDATA_SUFFIX = 'api/v1/charts/data?chart=netdata.plugin_pythond_nv'
+NETDATA_SUFFIX = '/api/v1/charts/data?chart=netdata.plugin_pythond_nv'
 DVE_BENCH_LIST = ''# TODO: ammend as API is determined
 
 # Local/Bluemix Deployment
@@ -23,6 +23,3 @@ TEMPLATE_DIR = 'templates/'
 HOME_PAGE = 'portal_home.html'
 SYS_PAGE = 'system_metrics.html'
 BENCH_PAGE = 'benchmark.html'
-
-# TODO: remove
-WHITE_LIST = [ { 'server' : '10.157.26.8' , 'netdata_port' : '5555' } ]
