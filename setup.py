967c7af6f591c92433348f12b8ebfe8de4ab6c20
Abigail Johnson <abigailjohnson@utexas.edu>
2017-03-28 01:14:09 -0400

    condense build

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
@@ -10,9 +9,20 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+# Dependencies
+requires = [
+    "Flask==0.11.1",
+    "cf-deployment-tracker==1.0.2"
+]
+
 setup(
     name='dellve-dash',
     version='1.0.0',
+    author='Abigail M. Johnson',
+    author_email='abigailjohnson@utexas.edu',
+    description='Front-end application to accompany the DellVE Benchmark Suite',
     long_description=long_description,
-    url='https://github.com/magescher/dellve-dash'
+    url='https://github.com/dellve/dellve-dash',
+    #url='http://dellve-dash.mybluemix.net',
+    install_requires=requires
 )
