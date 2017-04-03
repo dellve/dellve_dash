5ee265eb641abe80b51fb15641570dd91b37d58b
Abigail Johnson <abigailjohnson@utexas.edu>
2017-04-03 13:15:41 -0400

    stash

--- tests/test_test.py
+++ tests/test_test.py
@@ -15,6 +15,22 @@ invalid_config = '?'
 
 # Testing with LiveServer
 # TODO: Expand on tests (assert more than status code)
+#   Portal home:
+#       -Missing params results in alert box
+#       -valid params redirects to system overview page (and params present in url)
+#       -invalid params sends 422 and goes to invalid config page
+#   Invalid config page:
+#       -Portal home link redirects to portal home
+#       -Github link redirects to github
+#   Benchmark page:
+#       - start -> stop toggles ()
+#       - start -> system overview -> benchmarks -> asssert benchmark run config valid (stop button present)
+#       - prematurely stopped benchmark shows start button and yellow progress bar state
+#       - finished benchmark shows green progress bar with 'complete', start button, and run detail
+#   Sidebar:
+#       - Links link to correct page
+#
+
 class BasicTest(LiveServerTestCase):
     # if the create_app is not implemented NotImplementedError will be raised
     def create_app(self):
@@ -28,6 +44,7 @@ class BasicTest(LiveServerTestCase):
         self.app = dellve.app.test_client()
 
     # Basic test case - go to portal home
+    # TODO: Missing config
     def test_portal_home(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
@@ -39,12 +56,12 @@ class BasicTest(LiveServerTestCase):
     def test_benchmark_valid_config(self):
         response = self.app.get('/benchmarks' + valid_config)
         # TODO: once mock in place, replace with 200 assert (will fail when run from travis server as travis not behind vpn)
-        self.assertEqual(response.status_code, 422)
+        self.assertEqual(response.status_code, 200)
 
     def test_system_overview(self):
         response = self.app.get('/system-overview' + valid_config)
         # TODO: once mock in place, replace with 200 assert (will fail when run from travis server as travis not behind vpn)
-        self.assertEqual(response.status_code, 422)
+        self.assertEqual(response.status_code, 200)
 
     def test_invalid_progress_proxy(self):
         response = self.app.get('/progress-proxy' + invalid_config ) # no server config
@@ -53,4 +70,4 @@ class BasicTest(LiveServerTestCase):
     def test_valid_progress_proxy(self):
         response = self.app.get('/progress-proxy?url_base=10.157.26.8:9999')
         # TODO: once mock in place, replace with 200 assert (will fail when run from travis server as travis not behind vpn)
-        self.assertEqual(response.status_code, 422)
+        self.assertEqual(response.status_code, 200)
