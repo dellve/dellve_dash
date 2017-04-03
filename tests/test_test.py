2e8cfc904282d42b7faf82889f56a68c1afbe9f1
Abigail Johnson <abigailjohnson@utexas.edu>
2017-04-03 16:19:58 -0400

    typo fix

--- tests/test_test.py
+++ tests/test_test.py
@@ -2,7 +2,7 @@ import os
 import sys
 topdir = os.path.join(os.path.dirname(__file__), "..")
 sys.path.append(topdir)
-dellvedir = os.path.join(os.path.direname(__file__), ",,/dellve")
+dellvedir = os.path.join(os.path.dirname(__file__), "../dellve")
 sys.path.append(dellvedir)
 
 from dellve import dellve
