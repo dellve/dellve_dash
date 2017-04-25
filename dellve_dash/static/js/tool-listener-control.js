65489919d31de13f82c3e8d845ade07b409aa4df
Abigail Johnson <abigailjohnson@utexas.edu>
2017-04-25 11:10:31 -0400

    output parsing/formating update

--- dellve_dash/static/js/tool-listener-control.js
+++ dellve_dash/static/js/tool-listener-control.js
@@ -79,7 +79,9 @@ function updateRunDetail(runDetail) {
     // 2. Update Logging
     var logs = JSON.stringify(runDetail['output']);
     // Remove ANSI codes and misformed literals and styled header
-    logs = logs.replace(/['"]+/g, '').replace(/,/g , ' ').replace(/\\n/g, "<br>").replace(/\\t/g, "&nbsp;").replace(/\\u001b\[0m/g, "&nbsp;").replace(/\\u001b\[0;31m/g, "&nbsp;");
+    logs = logs.replace(/\\n/g, "<br>").replace(/\\t/g, "&nbsp;").replace(/\\u001b\[0m/g, "&nbsp;").replace(/\\u001b\[0;31m/g, "&nbsp;");
+    logs = logs.replace(/\\"/g, "'").replace(/","/g, "").replace(/"/g, "");
+    //logs = logs.replace(/['"]+/g, '');
     logs = logs.substring(1, logs.length-1);
     var header = "";
     if (runDetail['name']!= 'HPL') {
